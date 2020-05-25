e4b5b76316c77f15926dc66ab4b8a285db9f5883
luiscelano <luiscelano123@live.com>
2020-05-24 20:19:09 -0600

    adding homepage

---
+++ components/homepageNav.js
@@ -0,0 +1,20 @@
+document.getElementById('navbar').innerHTML=`
+<nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+    <div class="container">
+    <img src="assets/icono1v1.png" style="width:185px;">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="templates/index.html">Empezemos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about-us">Sobre nosotros</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+`
